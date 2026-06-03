eec4f843e8b64daaa21cee3b5b52cfcac5c47e34
Mridul-Works <mridul2431@gmail.com>
2026-06-03 22:26:04 +0530

    props topics started

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import HelloWorld from "./topics/jsx/HelloWorld"
 import Name from './topics/jsx/Name'
 import NestedComponent from './topics/components/NestedComponent'
+import TryProps from './topics/props/TryProps'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,11 @@ function App() {
 
     <h2>Below would be the nested component</h2>
     <NestedComponent count={"Mridul"} />
+    <TryProps
+  name="Xyz"
+  age={22}
+  location="Punjab"
+/>
     </>
   )
 }
---
+++ src/topics/props/TryProps.jsx
@@ -0,0 +1,12 @@
+ function TryProps(props){
+    return(
+        <>
+        <h1>Hello:</h1>
+        <h3>Name : {props.name}</h3>
+        <h3>age : {props.age}</h3>
+        <h3>location : {props.location}</h3>
+        </>
+    )
+ }
+
+ export default TryProps;
